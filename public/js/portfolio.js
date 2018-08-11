0f30f40a34cbdc485ba22afe4b63f7eb826c5ac1
ferre138 <jaymecunha@gmail.com>
2018-08-10 20:27:42 -0400

    Added Portfolio, Contact, and Footer
    Footer not complete

---
+++ public/js/portfolio.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+
+      if($(this).hasClass("nav-link")){
+      	if($(window).width() < 1024){
+      		$(".collapse").collapse();
+      	}
+      }
+    } // End if
+  });
+
+  // Add fade in to View Button inside .card
+  if($(window).width() > 576){
+		$(".card").hover(
+			function(){
+				// $(this).find(".btn").slideDown(500);
+				$(this).find(".btn").fadeIn(200, "linear");
+			}, function(){
+				// $(this).find(".btn").slideUp(500, "linear");
+				$(this).find(".btn").fadeOut(50, "linear");
+			}
+		);
+	};
+
+});
